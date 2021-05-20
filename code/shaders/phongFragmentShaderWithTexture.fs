3a80fbe6d799ac85ea83834f8f8925ba3b8f50a4
Joel Romero <joel.romero@enti.cat>
2021-05-20 17:13:54 +0200

    Molta cosa

--- code/shaders/phongFragmentShaderWithTexture.fs
+++ code/shaders/phongFragmentShaderWithTexture.fs
@@ -1,17 +1,13 @@
 #version 330
-	in vec2 _outUvs;
-	in vec3 _lightIntensity;
-	out vec4 out_Color;
-	uniform mat4 mv_Mat;
-	uniform vec4 lightPos;
-	uniform vec4 viewPos;
-	uniform vec4 lightColor;
-	uniform vec4 objectColor;
-	uniform sampler2D diffuseTexture;
-	
-	void main() {
-		vec4 textureColor = texture(diffuseTexture, _outUvs);
-		
-		out_Color = vec4(_lightIntensity, 1.0) * textureColor;
-		
-	}
+in vec4 vert_Normal;
+in vec2 vert_Tex;
+out vec4 out_Color;
+uniform mat4 mv_Mat;
+uniform vec4 color;
+uniform sampler2D tex;
+void main() {
+	out_Color = texture(tex, vert_Tex * vec2(1.0, 
+	-1.0))*0.5+ vec4(color.xyz * dot(vert_Normal, 
+	mv_Mat*vec4(0.0, 1.0, 0.0, 0.0))*0.3 + 
+	color.xyz * 0.2, 1.0 );
+}
