4206f4af3c3401772d62448ccc35d9e7985fee1a
Joel Romero <joel.romero@enti.cat>
2021-06-07 13:36:20 +0200

    Car windows done

--- code/shaders/phongFragmentShader.fs
+++ code/shaders/phongFragmentShader.fs
@@ -8,12 +8,15 @@
 	uniform vec4 viewPos;
 	uniform vec4 lightColor;
 	uniform vec4 objectColor;
+	uniform float alpha;
 	uniform sampler2D diffuseTexture;
 	
 	void main() {
-		vec4 textureColor = /*texture(diffuseTexture, _outUvs)*/texture(diffuseTexture, _outUvs * vec2(1.0, 
-	-1.0))*0.5;
-		
+		vec4 textureColor = texture(diffuseTexture, _outUvs * vec2(1.0, -1.0))*1.0;
+		float A;
+		if(alpha > 0) A = alpha - 0.01;
+		else A = alpha;
+		if(textureColor.a != 1) textureColor.a = alpha;
 		
 	
 		out_Color = vec4(_lightIntensity, 1.0) * textureColor;
